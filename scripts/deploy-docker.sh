dd170eccc72d57d870a625a281bc2cb0221990f2
stella6767 <stella@6767>
2025-03-13 23:40:18 +0900

    [test] action flow3

--- scripts/deploy-docker.sh
+++ scripts/deploy-docker.sh
@@ -5,7 +5,7 @@ echo "========================"
 echo "Path move"
 echo "========================"
 
-cd /home/ubuntu/cicd/
+cd /home/ubuntu/cicd/qr
 
 
 #openssl req -x509 -nodes -newkey rsa:2048 -keyout key.pem -out cert.pem -sha256 -days 365 \
@@ -18,7 +18,7 @@ echo "Docker compose down"
 echo "========================"
 
 # 이미 실행 중인 Docker Compose 중지 및 컨테이너 삭제
-sudo docker-compose -f /home/ubuntu/cicd/docker-compose-dev.yml down
+sudo docker-compose -f /home/ubuntu/cicd/docker-compose-qr.yml down
 
 # sudo docker-compose -f /Users/stella6767/IdeaProjects/free/docker-compose-qr.yml down
 
@@ -28,7 +28,7 @@ sudo docker-compose -f /home/ubuntu/cicd/docker-compose-dev.yml down
 #echo "========================"
 #
 #sudo docker-compose -f /Users/stella6767/IdeaProjects/free/docker-compose-qr.yml build
-sudo docker-compose -f /home/ubuntu/cicd/docker-compose-dev.yml build
+sudo docker-compose -f /home/ubuntu/cicd/docker-compose-qr.yml build
 
 echo " "
 echo "========================"
@@ -36,4 +36,4 @@ echo "Docker Compose Up"
 echo "========================"
 
 #sudo docker-compose -f /Users/stella6767/IdeaProjects/free/docker-compose-qr.yml up -d
-sudo docker-compose -f /home/ubuntu/cicd/docker-compose-dev.yml up -d
+sudo docker-compose -f /home/ubuntu/cicd/docker-compose-qr.yml up -d
