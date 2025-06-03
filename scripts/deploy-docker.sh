88fa0fc340a20a77d8df566dacef420b60ad6fb4
stella6767 <stella@6767>
2025-06-04 01:26:53 +0900

    [fix] deploy script

--- scripts/deploy-docker.sh
+++ scripts/deploy-docker.sh
@@ -18,22 +18,21 @@ echo "Docker compose down"
 echo "========================"
 
 # 이미 실행 중인 Docker Compose 중지 및 컨테이너 삭제
-sudo docker-compose -f /home/ubuntu/cicd/qr/docker-compose-qr.yml down
+sudo docker compose -f /home/ubuntu/cicd/qr/docker-compose-qr.yml down
 
-# sudo docker-compose -f /Users/stella6767/IdeaProjects/free/docker-compose-qr.yml down
 
-#echo " "
-#echo "========================"
-#echo "Docker compose build"
-#echo "========================"
-#
-#sudo docker-compose -f /Users/stella6767/IdeaProjects/free/docker-compose-qr.yml build
-sudo docker-compose -f /home/ubuntu/cicd/qr/docker-compose-qr.yml build
+echo " "
+echo "========================"
+echo "Docker compose build"
+echo "========================"
+
+
+sudo docker compose -f /home/ubuntu/cicd/qr/docker-compose-qr.yml build
 
 echo " "
 echo "========================"
 echo "Docker Compose Up"
 echo "========================"
 
-#sudo docker-compose -f /Users/stella6767/IdeaProjects/free/docker-compose-qr.yml up -d
-sudo docker-compose -f /home/ubuntu/cicd/qr/docker-compose-qr.yml up -d
+
+sudo docker compose -f /home/ubuntu/cicd/qr/docker-compose-qr.yml up -d
